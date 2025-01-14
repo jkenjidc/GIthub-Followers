fa6436caefc00453cb87bed65d929d7670cec237
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2025-01-14 15:59:46 -0500

    initial commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Kenji Dela Cruz on 1/7/25.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GHFollowers/FollowerListVC.swift
@@ -0,0 +1,22 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by Kenji Dela Cruz on 1/9/25.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    var userName: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        // Do any additional setup after loading the view.
+    }
+
+}
